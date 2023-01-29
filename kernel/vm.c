a54cf1b0571982ab6f34d64800f53275f5d3feb2
Danish Makbari <danish.mc2@gmail.com>
2023-01-29 17:38:40 +0300

    vmprint()

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,36 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable)
+{
+	printf("page table %p\n", pagetable);
+	for (int i = 0; i < 512; i++) {
+		pte_t p1 = pagetable[i];
+		if (p1 & PTE_V) {
+			printf(" ..%d: pte %p pa %p\n",
+				i,
+				p1,
+				PTE2PA(p1));
+			for (int j = 0; j < 512; j++) {
+				pte_t p2 = ((pagetable_t) PTE2PA(p1))[j];
+				if (p2 & PTE_V) {
+					printf(" .. ..%d: pte %p pa %p\n",
+						j,
+						p2,
+						PTE2PA(p2));
+					for (int k = 0; k < 512; k++) {
+						pte_t p3 = ((pagetable_t) PTE2PA(p2))[k];
+						if (p3 & PTE_V) {
+							printf(" .. .. ..%d: pte %p pa %p\n",
+								k,
+								p3,
+								PTE2PA(p3));
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
